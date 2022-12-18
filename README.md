286f3f0faffa479744e265d6033e0c189bf37aaa
bloodwolf <pshook456@gmail.com>
2022-12-18 15:15:03 -0600

    first commit -m

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # explorer
+
 RPGA PI
---
+++ src/test/java/com/example/explorer/ExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.explorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
