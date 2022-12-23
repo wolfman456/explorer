dfdfa684d831605f6b4889f9982bbc642ac1707d
bloodwolf <pshook456@gmail.com>
2022-12-23 00:06:29 -0600

    updates

--- src/main/java/com/example/explorer/user/user_repo/RoleRepository.java
+++ src/main/java/com/example/explorer/user/user_repo/RoleRepository.java
@@ -1,11 +1,12 @@
 package com.example.explorer.user.user_repo;
 
 
+import com.example.explorer.user.User_model.ERole;
 import com.example.explorer.user.User_model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(Enum name);
+    Optional<Role> findByName(ERole name);
 }
