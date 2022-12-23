2986820fcdad45bbc083a94d291e3fa3521e737e
bloodwolf <pshook456@gmail.com>
2022-12-22 23:58:14 -0600

    changes

--- src/main/java/com/example/explorer/user/user_repo/RoleRepository.java
+++ src/main/java/com/example/explorer/user/user_repo/RoleRepository.java
@@ -1,8 +1,10 @@
 package com.example.explorer.user.user_repo;
 
 
+import com.example.explorer.user.User_model.ERole;
 import com.example.explorer.user.User_model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
