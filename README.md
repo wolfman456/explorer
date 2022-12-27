f479ff68adcc8849028a1fadce0135640ef07e53
bloodwolf <pshook456@gmail.com>
2022-12-26 22:38:13 -0600

    update to user stories

--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 
 RPG API
 
-#User Stories :
+# User Stories :
+* ### User :
+  1. As a User I want to be able to create an account.
+  2. As a user I want to be able to create multiple avatars. 
+  3. As a user I want to be able to pick a class and race for my avatars.
 
-1. As a User I want to be able to create an account.
-2. As a user I want to be able to create multiple avatars. 
+* ### Admin :
+  1. As a admin I want to be able to create new races.
+  2. As a admin I want to be able to create new player classes.
+  3. As a admin I want to be able to Create new Items.
+
+* ### Moderator :
+  1. As a Moderator I want to be able to see a list of users.
+  2. As a Moderator I want to be able to suspend a User.
--- src/main/java/com/example/explorer/Character/model/UserCharacterResponse.java
+++ src/main/java/com/example/explorer/Character/model/UserCharacterResponse.java
@@ -1,33 +1,18 @@
 package com.example.explorer.Character.model;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonRootName(value = "user_char_response")
 public class UserCharacterResponse {
     private Race race;
     private UserChar userChar;
 
-    public UserCharacterResponse(Race race, UserChar userChar) {
-        this.race = race;
-        this.userChar = userChar;
-    }
-
-    public UserCharacterResponse() {
-    }
-
-    public Race getRace() {
-        return race;
-    }
-
-    public void setRace(Race race) {
-        this.race = race;
-    }
-
-    public UserChar getUserChar() {
-        return userChar;
-    }
-
-    public void setUserChar(UserChar userChar) {
-        this.userChar = userChar;
-    }
 }
