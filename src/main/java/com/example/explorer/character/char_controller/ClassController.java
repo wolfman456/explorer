b1213439df16d33886678c65cbd63e37bcb54bf8
wolfman456 <pshook456@gmail.com>
2023-01-08 12:40:11 -0600

    gitall added to class controller

--- src/main/java/com/example/explorer/character/char_controller/ClassController.java
+++ src/main/java/com/example/explorer/character/char_controller/ClassController.java
@@ -1,16 +1,17 @@
 package com.example.explorer.character.char_controller;
 
 import com.example.explorer.character.character_serv.ClassService;
+import com.example.explorer.character.model.PlayerClasses;
 import com.example.explorer.character.model.user_char_dto.ClassDTO;
 import com.example.explorer.exception.InformationExistException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/admin/player-class")
@@ -39,6 +40,17 @@ public class ClassController {
         }
 
         return ResponseEntity.internalServerError().build();
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<?> getAllClasses(){
+        try {
+            List<PlayerClasses> playerClasses = classService.getAllClasses();
+            return ResponseEntity.ok(playerClasses);
+        }catch (Exception exception){
+            logger.debug("getAllClasses failed with messages " +exception.getMessage());
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
 }
