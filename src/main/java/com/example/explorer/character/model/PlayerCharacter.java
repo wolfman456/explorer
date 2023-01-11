c7cf261870e9b3757c167df6bf4f74d779e24ba6
wolfman456 <87153905+wolfman456@users.noreply.github.com>
2023-01-10 19:59:56 -0600

    Class (#18)
    * Develop (#17)
    
    * update to auth service
    
    * update to user stories
    
    * Race unit test (#11)
    
    * Develop (#9)
    
    * update to auth service
    
    * update to user stories
    
    * first test classes created
    
    * Readme (#10)
    
    * update to auth service
    
    * update to user stories
    
    * frame work of readme layed out
    
    * update to readme
    
    * raceservice unit tests added
    
    * merge issue
    
    * Race unit test (#14)
    
    * first test classes created
    
    * raceservice unit tests added
    
    * Race unit test (#16)
    
    * first test classes created
    
    * raceservice unit tests added
    
    * Delete data directory
    
    * Develop (#15)
    
    * update to auth service
    
    * update to user stories
    
    * Race unit test (#11)
    
    * Develop (#9)
    
    * update to auth service
    
    * update to user stories
    
    * first test classes created
    
    * Readme (#10)
    
    * update to auth service
    
    * update to user stories
    
    * frame work of readme layed out
    
    * update to readme
    
    * raceservice unit tests added
    
    * merge issue
    
    * Race unit test (#14)
    
    * first test classes created
    
    * raceservice unit tests added
    
    * added file for controller and repo test
    
    * tests for race service and respository
    
    * controller test getAllRaces
    
    * race controller create tests added
    
    * minor update to race controller and service
    
    * update race test added
    
    Co-authored-by: bloodwolf <bloodwolf@pop-os.localdomain>
    
    Co-authored-by: bloodwolf <bloodwolf@pop-os.localdomain>
    
    * updated erd
    
    * player class added
    
    * refractoring
    
    * update to class
    
    * update to player class service
    
    * update ERD
    
    * class service completed
    
    * gitall added to class controller
    
    * class controller completed
    
    Co-authored-by: bloodwolf <bloodwolf@pop-os.localdomain>

---
+++ src/main/java/com/example/explorer/character/model/PlayerCharacter.java
@@ -0,0 +1,45 @@
+package com.example.explorer.character.model;
+
+import com.fasterxml.jackson.annotation.*;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonRootName(value = "player_charter")
+@Entity
+@Table(name = "player_charter")
+public class PlayerCharacter{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Long charId;
+    @Column(nullable = false, updatable = false)
+    private String charName;
+    @Column
+    private Integer wis;
+    @Column
+    private Integer str;
+    @Column
+    private Integer charisma;
+    @Column
+    private Integer con;
+    @Column Integer intelligent;
+    @Column
+    private Integer hitPoints;
+    @Column
+    private Integer baseAct;
+    @Column
+    private Integer baseSpellPoints;
+
+    @ManyToOne
+    @JoinColumn(name = "race_id")
+    private Race race;
+    @ManyToOne
+    @JoinColumn(name = "class_id")
+    private PlayerClasses playerClasses;
+
+}
