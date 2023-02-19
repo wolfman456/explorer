5d7359812f845df85fd55bfdfe55845470ac872a
wolfman456 <pshook456@gmail.com>
2023-02-19 01:14:01 -0600

    updeate to item service

--- src/main/java/com/example/explorer/items/item_serv/item_serv_impl/ItemServImpl.java
+++ src/main/java/com/example/explorer/items/item_serv/item_serv_impl/ItemServImpl.java
@@ -5,9 +5,14 @@ import com.example.explorer.items.item_serv.ItemServ;
 import com.example.explorer.items.model.Item;
 import com.example.explorer.items.model.dto.ItemDTO;
 import com.example.explorer.utility.CustomerMapper;
+import com.example.explorer.utility.ExplorerResponse;
+import com.example.explorer.utility.exception.InformationExistException;
+import com.example.explorer.utility.exception.InformationNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -19,6 +24,8 @@ public class ItemServImpl implements ItemServ {
     @Autowired
     CustomerMapper customerMapper;
 
+    private ExplorerResponse explorerResponse;
+
     @Override
     public List<Item> getAllItems() {
 
@@ -27,12 +34,29 @@ public class ItemServImpl implements ItemServ {
 
     @Override
     public String getItemByName(String name) {
-        return null;
+        Item item = itemRepo.findItemByName(name);
+        if (item==null){
+            throw new InformationNotFoundException(HttpStatus.NOT_FOUND, "no item with name " +name + " found", LocalDateTime.now());
+        }
+        explorerResponse.setItem(item);
+        return customerMapper.mapper(explorerResponse);
     }
 
     @Override
     public String createItem(ItemDTO itemDTO) {
-        return null;
+        if (itemRepo.findItemByName(itemDTO.getName()) != null){
+            throw new InformationExistException(HttpStatus.BAD_REQUEST, "iten with name " + itemDTO.getName() + " already exists", LocalDateTime.now());
+        }
+        Item item = Item.builder().name(itemDTO.getName())
+                .uniqueItem(itemDTO.getUniqueItem())
+                .description(itemDTO.getDescription())
+                .effect(itemDTO.getEffect())
+                .magic(itemDTO.getMagic())
+                .weight(itemDTO.getWeight())
+                .maxStackAmount(itemDTO.getMaxStackAmount()).build();
+        itemRepo.save(item);
+        explorerResponse.setItem(item);
+        return customerMapper.mapper(explorerResponse);
     }
 
     @Override
