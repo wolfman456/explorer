b97df39f72fb493473ecf9c15507e47d39ec6fc0
wolfman456 <pshook456@gmail.com>
2023-05-05 23:39:46 -0500

    update item completed

--- src/main/java/com/example/explorer/items/item_serv/item_serv_impl/ItemServImpl.java
+++ src/main/java/com/example/explorer/items/item_serv/item_serv_impl/ItemServImpl.java
@@ -61,7 +61,36 @@ public class ItemServImpl implements ItemServ {
 
     @Override
     public String updateItem(ItemDTO itemDTO, String name) {
-        return null;
+        Item item = itemRepo.findItemByName(name);
+        if (item != null){
+            if (itemDTO.getUniqueItem() != null){
+                item.setUniqueItem(itemDTO.getUniqueItem());
+            }
+            if (itemDTO.getMagic() != null){
+                item.setMagic(itemDTO.getMagic());
+            }
+            if (itemDTO.getName() != null){
+                item.setName(itemDTO.getName());
+            }
+            if (itemDTO.getEffect() != null){
+                item.setEffect(itemDTO.getEffect());
+            }
+            if (itemDTO.getWeight() != null){
+                item.setWeight(itemDTO.getWeight());
+            }
+            if (itemDTO.getDescription() != null){
+                item.setDescription(itemDTO.getDescription());
+            }
+            if (itemDTO.getMaxStackAmount() != null){
+                item.setMaxStackAmount(itemDTO.getMaxStackAmount());
+            }
+            itemRepo.save(item);
+            explorerResponse.setItem(item);
+            return customerMapper.mapper(explorerResponse);
+
+        }else{
+            throw new InformationNotFoundException(HttpStatus.NOT_FOUND, "item with name " + itemDTO.getName() + " was not found", LocalDateTime.now());
+        }
     }
 
     @Override
